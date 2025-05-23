b37634d26e25c5ca5cd963c901b5ddf5af5a059e
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-05-23 14:52:04 -0400

    macros

---
+++ language/minimal.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define fore(i, a, b) for(int i = (a); i<(b); i++)
+#define FOR(i, n) for(int i = 0; i<(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+using vi = vector<int>;
+const int MOD = 1000000007;
+const ll INF = 9223372036854775807LL;
+const int tam = 1;
+
+void solve(){
+   
+}
+
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
