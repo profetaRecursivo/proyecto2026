ee71d1fc20393d63112d838567c7331ec19282ca
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-07-20 23:16:01 -0400

    trucos del compilador

---
+++ tricks/compiler.sh
@@ -0,0 +1,3 @@
+g++ -std=c++11 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o a a.cpp
+g++ -std=c++20 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o a a.cpp
+g++ -std=c++23 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o a a.cpp
