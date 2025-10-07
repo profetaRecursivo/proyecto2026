62ad6c9694d7be75a765e730f5d0b6f2fa249d6b
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-10-07 19:40:58 -0400

    cheatsheets de geometria

--- geometria/comparadores.cpp
+++ geometria/comparadores.cpp
@@ -1,4 +1,4 @@
-bool ge(type x, type y) { return x + EPS > y; }
-bool le(type x, type y) { return x - EPS < y; }
-bool eq(type x, type y) { return ge(x, y) and le(x, y); }
-int sign(type x) { return ge(x, 0) - le(x, 0); }
+bool ge(double x, double y) { return x + EPS > y; }
+bool le(double x, double y) { return x - EPS < y; }
+bool eq(double x, double y) { return ge(x, y) and le(x, y); }
+int sign(double x) { return ge(x, 0) - le(x, 0); }
