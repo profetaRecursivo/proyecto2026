e244bb609e9c2a37768c1f6018ea1282ca9f9c22
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-10-07 19:42:11 -0400

    mejora del hash

--- strings/hashing.cpp
+++ strings/hashing.cpp
@@ -1,23 +1,38 @@
-#define bint __int128 //indexado a 1
+#define bint __int128  // indexado a 1
 struct Hash {
-  bint MOD=212345678987654321LL, P=1777771, 
-  PI=106955741089659571LL;
-  vector<bint> h, pi;
-  Hash(string s) {
-    s = ' ' + s; 
-    int n = s.size() - 1; 
-    assert((P * PI) % MOD == 1);
-    h.resize(n + 2); pi.resize(n + 2);
-    h[0] = 0; pi[0] = 1;
-    bint p = 1;
-    for (int i = 1; i <= n; ++i) {
-      h[i] = (h[i - 1] + p * s[i]) % MOD;
-      pi[i] = (pi[i - 1] * PI) % MOD;
-      p = (p * P) % MOD;
-    }
-  }
+	bint MOD = 212345678987654321LL, P = 1777771, PI = 106955741089659571LL;
+	vector<bint> h, pi, pows;
+	Hash() {}
+	Hash(string s) {
+		s = ' ' + s;
+		int n = s.size() - 1;
+		//assert((P * PI) % MOD == 1);
+		h.resize(n + 2);
+		pi.resize(n + 2);
+		h[0] = 0;
+		pi[0] = 1;
+		bint p = 1;
+		pows.resize(n + 2);
+		pows[0] = 1;
+		for (int i = 1; i <= n; ++i) {
+			h[i] = (h[i - 1] + p * s[i]) % MOD;
+			pi[i] = (pi[i - 1] * PI) % MOD;
+			p = (p * P) % MOD;
+			pows[i] = (pows[i - 1] * P) % MOD;
+		}
+	}
 
-  long long get(int l, int r) {
-    return (((h[r] - h[l - 1] + MOD) % MOD) * pi[l - 1]) % MOD;
-  }
+	ll get(int l, int r) const {
+		return (((h[r] - h[l - 1] + MOD) % MOD) * pi[l - 1]) % MOD;
+	}
+	ll modify(int idx, char nuevo) const {
+		int n = sz(h) - 2;
+		bint Hraw = h[n];
+		ll origVal = get(idx, idx);
+		bint pow = pows[idx - 1];
+		bint nuevoRaw =
+			Hraw - (bint)origVal * pow % MOD + (bint)(nuevo)*pow % MOD;
+		nuevoRaw = (nuevoRaw % MOD + MOD) % MOD;
+		return (ll)nuevoRaw;
+	}
 };
