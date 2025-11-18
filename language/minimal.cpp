b08210c8c4fe1153efd4df6e453c0f99d9206696
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-11-18 17:50:37 -0400

    update template

--- language/minimal.cpp
+++ language/minimal.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
-#define fore(i, a, b) for(int i = (a); i<(b); i++)
-#define FOR(i, n) for(int i = 0; i<(n); i++)
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
-#define pb push_back
-using vi = vector<int>;
+#define dbg(x) cerr<<#x<<" = "<<x<<' '
 const int MOD = 1000000007;
-const ll INF = 9223372036854775807LL;
+const ll INF = 4e18;
 const int tam = 1;
 
 void solve(){
-   
+    
 }
 
 signed main(){
