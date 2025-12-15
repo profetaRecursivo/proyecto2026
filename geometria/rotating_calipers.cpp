08aac2758ac2be90ddcf280f98878612a38b00e3
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-12-14 23:12:09 -0400

    rc

--- geometria/rotating_calipers.cpp
+++ geometria/rotating_calipers.cpp
@@ -1,7 +1,7 @@
 ll cross(const pto& a, const pto& b, const pto& c) { 
     return (b - a) % (c - a); 
 }
-
+//maxima distancia entre dos puntos de un polygono convexo
 ll rc(const vector<pto>& hull) {
 	int j = 1;
 	ll max_dist2 = 0;
@@ -28,4 +28,20 @@ ll rc(const vector<pto>& hull) {
 		}
 	}
 	return max_dist2;
+}
+
+//minima distancia entre dos polygonos
+ll rc(const vector<Punto>& poly1, const vector<Punto>& poly2) {
+    int n = sz(poly1), m = sz(poly2);
+    ll min_dist = INF;
+    int i = 0, j = 0;
+    for (int k = 0; k < n; k++) {
+        while (cross(poly1[i], poly2[j], poly2[(j + 1) % m]) > 0) {
+            j = (j + 1) % m;
+        }
+        ll dist = (poly1[i] - poly2[j]).dist2();
+        min_dist = min(min_dist, dist);
+        i = (i + 1) % n;
+    }
+    return min_dist;
 }
