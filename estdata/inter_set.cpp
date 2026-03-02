f145a564a39c94f0a0f07effba5498525f1bebf5
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2026-03-02 16:20:15 -0400

    improve interset

--- estdata/inter_set.cpp
+++ estdata/inter_set.cpp
@@ -34,4 +34,26 @@ struct inter_set {
         }
         return {INF, INF};
     }
+    void erase(int l, int r) {
+        if (l > r) return;
+        auto it = inter.upper_bound({l, INF});
+        if (it != inter.begin()) it--;
+        vector<pair<int,int>> to_add;
+        while (it != inter.end() and it->first <= r) {
+            int a = it->first, b = it->second;
+            if (b < l) {
+                it++;
+                continue;
+            }
+            auto it_erase = it++;
+            inter.erase(it_erase);
+            if (a < l) {
+                to_add.emplace_back(a, l - 1);
+            }
+            if (b > r) {
+                to_add.emplace_back(r + 1, b);
+            }
+        }
+        for (auto &p : to_add) inter.insert(p);
+    }
 };
